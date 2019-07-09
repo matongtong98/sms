4a17b3df2015cd7303135d1d24203b7cf68f12aa
吕健强 <1060784557@qq.com>
2019-07-09 17:02:43 +0800

    [吕小强]uesr_role 创建

---
+++ src/main/java/com/briup/apps/sms/bean/user_role.java
@@ -0,0 +1,26 @@
+package com.briup.apps.sms.bean;
+/*user_role表*/
+public class user_role {
+	private long id;
+	private long user_id;
+	private long role_id;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public long getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(long user_id) {
+		this.user_id = user_id;
+	}
+	public long getRole_id() {
+		return role_id;
+	}
+	public void setRole_id(long role_id) {
+		this.role_id = role_id;
+	}
+
+}
