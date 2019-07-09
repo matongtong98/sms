61c5f141481b126471732216dfbf3b6f1399afe7
马佟佟 <1136487068@qq.com>
2019-07-09 16:29:20 +0800

    [马佟佟]school表的创建

--- src/main/java/com/briup/apps/sms/bean/School.java
+++ src/main/java/com/briup/apps/sms/bean/School.java
@@ -4,53 +4,39 @@ package com.briup.apps.sms.bean;
  **/
 public class School {
   private Long id;
-  private String realname;
-  private String telephone;
-  private String password;
-  private String student_no;
-  private String gender;
-  private Long clazz_id;
+  private String name;
+  private String description;
+  private String tel;
+  private String address;
 public Long getId() {
 	return id;
 }
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getDescription() {
+	return description;
+}
+public void setDescription(String description) {
+	this.description = description;
+}
+public String getTel() {
+	return tel;
+}
+public void setTel(String tel) {
+	this.tel = tel;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
 public void setId(Long id) {
 	this.id = id;
-}
-public String getRealname() {
-	return realname;
-}
-public void setRealname(String realname) {
-	this.realname = realname;
-}
-public String getTelephone() {
-	return telephone;
-}
-public void setTelephone(String telephone) {
-	this.telephone = telephone;
-}
-public String getPassword() {
-	return password;
-}
-public void setPassword(String password) {
-	this.password = password;
-}
-public String getStudent_no() {
-	return student_no;
-}
-public void setStudent_no(String student_no) {
-	this.student_no = student_no;
-}
-public String getGender() {
-	return gender;
-}
-public void setGender(String gender) {
-	this.gender = gender;
-}
-public Long getClazz_id() {
-	return clazz_id;
-}
-public void setClazz_id(Long clazz_id) {
-	this.clazz_id = clazz_id;
 }
 
 }
