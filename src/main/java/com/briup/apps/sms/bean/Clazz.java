dd55d0c1b4561b5eabc4ac2935e3ee40b646c31b
姚宁 <1753871306@qq.com>
2019-07-09 17:16:53 +0800

    [姚宁]-Clazz的创建

---
+++ src/main/java/com/briup/apps/sms/bean/Clazz.java
@@ -0,0 +1,34 @@
+package com.briup.apps.sms.bean;
+
+public class Clazz {
+	private  int  id;
+	private  char name;
+	private  int  begin_time;
+	private  int  college_id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public char getName() {
+		return name;
+	}
+	public void setName(char name) {
+		this.name = name;
+	}
+	public int getBegin_time() {
+		return begin_time;
+	}
+	public void setBegin_time(int begin_time) {
+		this.begin_time = begin_time;
+	}
+	public int getCollege_id() {
+		return college_id;
+	}
+	public void setCollege_id(int college_id) {
+		this.college_id = college_id;
+	}
+	
+
+}
