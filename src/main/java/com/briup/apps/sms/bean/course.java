b5362f59c54fec6ac4e527ea27776204618a2b60
牟金杰 <1339849483@qq.com>
2019-07-09 17:13:34 +0800

    [牟金杰]course表及部分代码

--- src/main/java/com/briup/apps/sms/bean/course.java
+++ src/main/java/com/briup/apps/sms/bean/course.java
@@ -1,42 +1,5 @@
 package com.briup.apps.sms.bean;
 
 public class course {
-	  private Long id;
-	  private String name;
-	  private String credit;
-	  private long user_id;
-	  
-	  public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getCredit() {
-		return credit;
-	}
-
-	public void setCredit(String credit) {
-		this.credit = credit;
-	}
-
-	public long getUser_id() {
-		return user_id;
-	}
-
-	public void setUser_id(long user_id) {
-		this.user_id = user_id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public long getId() {
-		  
-		return id;
 
 }
-	  }
