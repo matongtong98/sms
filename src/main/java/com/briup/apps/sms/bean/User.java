11adde9aa6495a411cdf6b481fd2002d25d1b79f
卢伟志 <lwz847317201>
2019-07-09 17:03:30 +0800

    [卢伟志]User的创建

---
+++ src/main/java/com/briup/apps/sms/bean/User.java
@@ -0,0 +1,54 @@
+package com.briup.apps.sms.bean;
+
+public class User {
+     private Long id;
+     private String realname;
+     private String telephone;
+     private String password;
+     private String student_no;
+     private String gender;
+     private Long clazz_id;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getRealname() {
+		return realname;
+	}
+	public void setRealname(String realname) {
+		this.realname = realname;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getStudent_no() {
+		return student_no;
+	}
+	public void setStudent_no(String student_no) {
+		this.student_no = student_no;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public Long getClazz_id() {
+		return clazz_id;
+	}
+	public void setClazz_id(Long clazz_id) {
+		this.clazz_id = clazz_id;
+	}
+     
+}
