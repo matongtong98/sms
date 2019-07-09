97e70687771d0cdeaf62819d39693a7215494117
吕健强 <1060784557@qq.com>
2019-07-09 17:12:42 +0800

    [吕健强]表数据跟新

--- src/main/java/com/briup/apps/sms/dao/user_roledao.java
+++ src/main/java/com/briup/apps/sms/dao/user_roledao.java
@@ -1,5 +1,5 @@
 package com.briup.apps.sms.dao;
-
+/*user_role表*/
 public class user_roledao {
 
 }
--- src/main/java/com/briup/apps/sms/service/user_roleservice.java
+++ src/main/java/com/briup/apps/sms/service/user_roleservice.java
@@ -1,5 +1,5 @@
 package com.briup.apps.sms.service;
-
+/*user_role表*/
 public interface user_roleservice {
 
 }
