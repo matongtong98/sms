1857c491d08d0ce1bda4237a3e832c92c5544052
乔开斌 <893998436@qq.com>
2019-07-10 11:04:11 +0800

    []数据恢复

---
+++ src/main/java/com/briup/apps/sms/bean/College.java
@@ -0,0 +1,32 @@
+package com.briup.apps.sms.bean;
+
+public class College {
+	private Long id;
+	private String name;
+	private String description;
+	private Long school_id;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Long getSchool_id() {
+		return school_id;
+	}
+	public void setSchool_id(Long school_id) {
+		this.school_id = school_id;
+	}
+}
