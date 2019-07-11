3121c93af775d906f00cf5e278ea2f51809a7384
卢昌昊 <330130449@qq.com>
2019-07-11 13:31:53 +0800

    [身份信息错误修改]卢昌昊

--- src/main/java/com/briup/apps/sms/web/controller/RoleController.java
+++ src/main/java/com/briup/apps/sms/web/controller/RoleController.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.briup.apps.sms.bean.Clazz;
-import com.briup.apps.sms.service.Clazzservice;
+import com.briup.apps.sms.bean.Role;
+import com.briup.apps.sms.service.Roleservice;
 @RestController
 @RequestMapping("/role")
 public class RoleController {
@@ -13,7 +13,7 @@ public class RoleController {
 	@Autowired
 	private Roleservice roleservice;
 	
-	// http://localhost:8080/clazz/selectAll
+	// http://localhost:8080/role/selectAll
 	@GetMapping("selectAll")
 	public List<Role> selectAll(){
 		
