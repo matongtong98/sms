3d9f06661d60884b8eb95d52ca509727e2a6b199
姚宁 <1753871306@qq.com>
2019-07-12 08:18:11 +0800

    [姚宁]补充

--- src/main/java/com/briup/apps/sms/dao/ClazzDao.java
+++ src/main/java/com/briup/apps/sms/dao/ClazzDao.java
@@ -5,15 +5,16 @@ import java.util.List;
 import com.briup.apps.sms.bean.Clazz;
 public interface ClazzDao {
 	
-		//查询所有的
+		//查询所有
 		List<Clazz> selectAll();
-		     
+		
 		//插入
 		void insert(Clazz clazz);
 		
 		//修改
 		void update(Clazz clazz);
-		
+       //删除操作
 		void deleteById(long id);
+		
 
 }
